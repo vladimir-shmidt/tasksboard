9a2f276ec2d78200fdcdd804e0c443f4251f243b
Vladimir Shmidt <vladimir@shmidt.me>
2018-09-23 23:42:20 +0300

    add action buttons for task view

--- tasksfront/src/app/taskslist/taskslist.component.ts
+++ tasksfront/src/app/taskslist/taskslist.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, TemplateRef, ViewChild, OnInit } from '@angular/core';
 import { Task } from '../task';
 import { TaskSelectedService } from '../TaskSelectedService';
 import { Router } from "@angular/router";
@@ -9,23 +9,26 @@ import { Router } from "@angular/router";
   styleUrls: ['./taskslist.component.css']
 })
 export class TasksListComponent implements OnInit {
+  @ViewChild('editTmpl') editTmpl: TemplateRef<any>;
+
   tasks: Task[] = [];
-  rows = [
-    { Id: "1", Name: "1", Description: "One", Priority: 1, Timespan: Date.now(), Status: 'Active'},
-    { Id: "2", Name: "2", Description: "Two", Priority: 2, Timespan: Date.now()},
-    { Id: "3", Name: "3", Description: "Three", Priority: 1, Timespan: Date.now()},
-  ];
-  columns = [
-    { prop: 'Name' },
-    { prop: 'Priority' },
-    { prop: 'Created', name: 'Added' },
-    { prop: 'Timespan' },
-    { prop: 'Status' }
-  ];
+  rows = [];
+  columns = [];
 
   constructor(private router: Router, private service: TaskSelectedService){ }
 
   ngOnInit() {
+    this.columns = [{ prop: 'Name' },
+    { prop: 'Priority' },
+    { prop: 'Created', name: 'Added' },
+    { prop: 'Timespan' },
+    { prop: 'Status' },
+    { name: 'Actions', prop: 'Status',  cellTemplate: this.editTmpl }];
+    this.rows = [
+      { Id: "1", Name: "1", Description: "One", Priority: 1, Timespan: Date.now(), Status: 'Active'},
+      { Id: "2", Name: "2", Description: "Two", Priority: 2, Timespan: Date.now()},
+      { Id: "3", Name: "3", Description: "Three", Priority: 1, Timespan: Date.now()},
+    ];
   }
 
   onSelect({ selected }) {
